cbf2213d8cd1b2401fc27844deacb07081a96e5c
mariohood <manhattan199@hotmail.com>
2022-07-05 18:40:18 -0300

    PW2-Auth-user implementado get Token para usuario

---
+++ auth-user/src/main/java/dev/ifrs/Auth.java
@@ -0,0 +1,36 @@
+package dev.ifrs;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+import javax.annotation.security.PermitAll;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.eclipse.microprofile.jwt.Claims;
+
+import io.smallrye.jwt.build.Jwt;
+
+@Path("/auth")
+public class Auth {
+    
+    @Path("/getjwt")
+    @POST
+    @PermitAll
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getJWT(@FormParam("name") String name, @FormParam("email") String email){
+        
+        return Jwt.issuer("http://localhost:8084")
+            .upn(email)
+            .groups(new HashSet<>(Arrays.asList("User")))
+            .expiresAt(System.currentTimeMillis() + 360000)
+            .claim(Claims.full_name, name)
+            .claim(Claims.email, email)
+            .sign();
+    }
+}
